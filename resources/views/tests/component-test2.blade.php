641dc1bd7e990f82428f9b6d51a747dafc7a060e
minami0601 <minami.love.bb@icloud.com>
2022-05-03 07:19:30 +0900

    classBasi

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,7 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー2</x-slot>
   コンポーネントテスト2
+  <x-test-class-base classBaseMessage="クラスベースのメッセージです"/>
+  <div class="mb-5"></div>
+  <x-test-class-base classBaseMessage="クラスベースのメッセージです" defaultMessage="初期値を変更しています"/>
 </x-tests.app>
